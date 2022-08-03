857930a73b5aa72ccfe50029b9910c5275746fe1
B. Scott Michel <scooter.phd@gmail.com>
2022-08-03 10:14:43 -0700

    Remove vestiges of the original project

--- scripts/make-userdb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-sqlite3 explan_udata.sqlite3 < scripts/explan_udata.sql
-sqlite3 explan_udata.sqlite3 "select sql from sqlite_schema;" | sha256sum
-sqlite3 explan_udata.sqlite3 "select sql from sqlite_schema;" > yyy.sql
